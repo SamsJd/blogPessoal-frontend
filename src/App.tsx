4f7e852f67f0f6e0e385868546a78c33e5b7197e
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-07-30 15:37:53 -0300

    Criando o Componente AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
@@ -9,18 +10,20 @@ import Login from './pages/login/Login';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/cadastro' element={<Cadastro />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path='/' element={<Login />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/cadastro' element={<Cadastro />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
