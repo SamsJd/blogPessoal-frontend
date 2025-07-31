249feed6cff617c27e29d69e306fa94493c12454
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-07-31 20:04:22 -0300

    Criando o Componente Deletar Tema

--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -20,10 +20,10 @@ function CardTemas({ tema }: CardTemasProps) {
           <button>Editar</button>
         </Link>
 
-        <Link to='' 
-              className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                        flex items-center justify-center'>
-              <button>Deletar</button>
+        <Link to={`/deletartema/${tema.id}`} 
+          className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+            flex items-center justify-center'>
+          <button>Deletar</button>
         </Link>
       </div>
     </div>
